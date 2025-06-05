ef037d65655998868326b0402d4ffde0a26bbe98
Philipe Cristiano <philipecristiano@gmail.com>
2025-06-04 21:06:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ if __name__ == "__main__":
 @app.route('/')
 def home():
     return render_template('index.html')
+            if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
